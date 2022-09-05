ba59693317048364b857b3d87117eaa5cf169499
wesleycremonini <wesley18.cre@gmail.com>
2022-09-05 18:49:27 -0300

    fix typo

--- app/Routing/Api.php
+++ app/Routing/Api.php
@@ -11,7 +11,7 @@ class Api
       echo 'Hello World';
     });
 
-    Router::get('/post/([0-9]*)', function (Request $req, Response $res) {
+    Router::get('/posts/([0-9]*)', function (Request $req, Response $res) {
       $res->toJSON([
         'post' =>  ['id' => $req->params[0]],
         'status' => 'ok'
