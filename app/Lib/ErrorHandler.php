02f8070d37626ce25d12699a169876f7cccc42b3
wesleycremonini <wesley18.cre@gmail.com>
2022-09-05 08:24:17 -0300

    update return type

--- app/Lib/ErrorHandler.php
+++ app/Lib/ErrorHandler.php
@@ -3,7 +3,7 @@
 namespace App\Lib;
 
 class ErrorHandler {
-  public static function run() {
+  public static function run(): void {
     $run = new \Whoops\Run;
     $handler = new \Whoops\Handler\PrettyPageHandler;
     
