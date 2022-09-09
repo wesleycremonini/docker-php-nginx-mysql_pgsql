a361ca9270fff2aba3946604de793aed28a0c48e
wesleycremonini <wesley18.cre@gmail.com>
2022-09-09 16:18:13 -0300

    db connection setup

--- app/Database/DB.php
+++ app/Database/DB.php
@@ -3,13 +3,15 @@
 namespace App\Database;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
 
 class DB
 {
     public static function connect(): void
     {
         $capsule = new Capsule;
-
+        
         $capsule->addConnection([
             'driver'    => 'mysql',
             'host'      => 'db',
@@ -20,7 +22,14 @@ class DB
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
         ]);
+        
+        // Set the event dispatcher used by Eloquent models... (optional)
+        $capsule->setEventDispatcher(new Dispatcher(new Container));
 
+        // Make this Capsule instance available globally via static methods... (optional)
         $capsule->setAsGlobal();
+
+        // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+        $capsule->bootEloquent();
     }
 }
