66b80f463e836018adcbf25a527cd60e596b2651
wesleycremonini <wesley18.cre@gmail.com>
2022-09-11 17:33:13 -0300

    update json response trait

--- app/Traits/JsonResponseTrait.php
+++ app/Traits/JsonResponseTrait.php
@@ -2,32 +2,49 @@
 
 namespace App\Traits;
 
+use Slim\Psr7\Response;
+
 trait JsonResponseTrait
 {
-  protected function success(array $data, int $statusCode)
+  protected function success(array $data, int $statusCode, Response $response): Response
   {
-    http_response_code($statusCode);
-    echo json_encode([
+    $payload = json_encode([
       "status" => "success",
       "data" => $data
     ]);
+
+    $response->getBody()->write($payload);
+
+    return $response
+      ->withHeader('Content-Type', 'application/json')
+      ->withStatus($statusCode);
   }
 
-  protected function fail(array $data, int $statusCode)
+  protected function fail(array $data, int $statusCode, Response $response): Response
   {
-    http_response_code($statusCode);
-    echo json_encode([
+    $payload = json_encode([
       "status" => "fail",
       "data" => $data
     ]);
+
+    $response->getBody()->write($payload);
+
+    return $response
+      ->withHeader('Content-Type', 'application/json')
+      ->withStatus($statusCode);
   }
 
-  protected function error(array $data, int $statusCode)
+  protected function error(array $data, int $statusCode, Response $response): Response
   {
-    http_response_code($statusCode);
-    echo json_encode([
+    $payload = json_encode([
       "status" => "error",
       "data" => $data
     ]);
+
+    $response->getBody()->write($payload);
+
+    return $response
+      ->withHeader('Content-Type', 'application/json')
+      ->withStatus($statusCode);
   }
 }
